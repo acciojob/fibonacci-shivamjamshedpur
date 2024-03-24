5c2c57d1bd99a2559eb14e9b426bce78bfdb1191
Shivam kumar <49615953+shivamjamshedpur@users.noreply.github.com>
2024-03-24 21:23:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,10 +11,6 @@ function fibonacci(n) {
   return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-// Print the first 10 Fibonacci numbers
-for (let i = 0; i < 10; i++) {
-  console.log(fibonacci(i));
-}
 
 
 module.exports = fibonacci;
